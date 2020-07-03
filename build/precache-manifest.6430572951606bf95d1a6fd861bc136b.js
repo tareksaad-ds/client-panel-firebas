55483032f1d91c736f2951b84385348bff25be30
Tarek <tarekmarketer30@gmail.com>
2020-07-03 20:38:28 +0200

    build app

---
+++ build/precache-manifest.6430572951606bf95d1a6fd861bc136b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5428ddcb8e7693c3941a277c61faf58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e59f0041a7cf101636a",
+    "url": "/static/js/2.3e464986.chunk.js"
+  },
+  {
+    "revision": "950a02c51c8aa986cd334551d96de72f",
+    "url": "/static/js/2.3e464986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66fa4d210376a9d95026",
+    "url": "/static/js/main.1fd1b7ce.chunk.js"
+  },
+  {
+    "revision": "81c594b70858d8c951cd",
+    "url": "/static/js/runtime-main.2190a81d.js"
+  }
+]);
